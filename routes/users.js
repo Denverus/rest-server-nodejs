51a7e7cb6dff3747a22c7f87f06be02cfbd18e6d
Denis Trotsky <denveru@gmail.com>
2016-05-09 23:50:22 -0700

    Complete test service for users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+var users = [];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    console.log(req.body);
+
+    res.json( users );
+});
+
+router.post('/', function(req, res, next) {
+    console.log(req.body);
+
+    var user = req.body;
+
+    users.push(user);
+    res.json( user );
+});
+
+
+router.delete('/', function(req, res, next) {
+    console.log(req.body);
+
+    users = [];
+    res.sendStatus(200);
 });
 
 module.exports = router;
